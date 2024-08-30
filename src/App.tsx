25db76f9f0c6b8b489b3bf7d7855f957e44c63e3
Virendra Patil <virendrapatil.24vp@gmail.com>
2024-08-30 23:16:33 +0530

    adds components to main app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-// import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
-// import viteLogo from "/vite.svg";
 import "./App.css";
+import NavBar from "./components/NavBar/NavBar";
+import SeedPhraseGenerator from "./components/SeedPhraseGenerator/SeedPhraseGenerator";
 
 function App() {
   return (
     <>
-      <div>CrypWal</div>
+      <div className="container">
+        <NavBar />
+        <SeedPhraseGenerator />
+      </div>
     </>
   );
 }
