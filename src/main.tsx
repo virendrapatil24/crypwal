a44cfd64978ec845f317b7620ea1651e3dfcac60
Virendra Patil <virendrapatil.24vp@gmail.com>
2024-08-30 21:00:46 +0530

    removes chakra ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { ChakraProvider } from "@chakra-ui/react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
@@ -6,8 +5,6 @@ import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <App />
   </StrictMode>
 );
