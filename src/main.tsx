81921572117b3a20260e9d70dbaee184101dafbb
Virendra Patil <virendrapatil.24vp@gmail.com>
2024-08-30 11:21:08 +0530

    adds chakra to the ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { ChakraProvider } from "@chakra-ui/react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
@@ -5,6 +6,8 @@ import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </StrictMode>
 );
