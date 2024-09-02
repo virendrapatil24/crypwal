c05fa77fdf28c9a6bca155319773843ff469a33d
Virendra Patil <virendrapatil.24vp@gmail.com>
2024-09-02 21:42:44 +0530

    minor fix

--- src/components/SolanaWalletGenerator/SolanaWalletGenerator.tsx
+++ src/components/SolanaWalletGenerator/SolanaWalletGenerator.tsx
@@ -37,7 +37,7 @@ const SolanaWalletGenerator = ({ seed }: seedProps) => {
         privateKey: privateKey,
         publicKey: publicKey,
         balance: balance,
-        privateKeyVisible: false,
+        privateKeyVisible: true,
       },
     ]);
     setCurrentIndex(currentIndex + 1);
