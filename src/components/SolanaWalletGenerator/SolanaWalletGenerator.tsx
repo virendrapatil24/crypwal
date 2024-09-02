91739856fb65decf610a8569ad031369f70c9c75
Virendra Patil <virendrapatil.24vp@gmail.com>
2024-09-02 21:02:03 +0530

    adds solana wallet balance

--- src/components/SolanaWalletGenerator/SolanaWalletGenerator.tsx
+++ src/components/SolanaWalletGenerator/SolanaWalletGenerator.tsx
@@ -4,6 +4,7 @@ import { derivePath } from "ed25519-hd-key";
 import nacl from "tweetnacl";
 import { Keypair } from "@solana/web3.js";
 import bs58 from "bs58";
+import axios from "axios";
 
 interface seedProps {
   seed: Buffer;
@@ -13,6 +14,7 @@ interface SolanaWallet {
   index: number;
   privateKey: string;
   publicKey: string;
+  balance: number;
   privateKeyVisible: boolean;
 }
 
@@ -20,19 +22,21 @@ const SolanaWalletGenerator = ({ seed }: seedProps) => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [solanaWallets, setSolanaWallets] = useState<SolanaWallet[]>([]);
 
-  const generateSolanaWallet = () => {
+  const generateSolanaWallet = async () => {
     const path = `m/44'/501'/${currentIndex}'/0'`;
     const derivedSeed = derivePath(path, seed.toString("hex")).key;
     const secretKey = nacl.sign.keyPair.fromSeed(derivedSeed).secretKey;
     const keypair = Keypair.fromSecretKey(secretKey);
     const privateKey = bs58.encode(secretKey);
     const publicKey = keypair.publicKey.toBase58();
+    const balance = await getSolanaBalance(publicKey);
     setSolanaWallets((prevWallets) => [
       ...prevWallets,
       {
         index: currentIndex,
         privateKey: privateKey,
         publicKey: publicKey,
+        balance: balance,
         privateKeyVisible: false,
       },
     ]);
@@ -42,6 +46,25 @@ const SolanaWalletGenerator = ({ seed }: seedProps) => {
   const deleteWallet = (idx: number) => {
     const updatedWallets = solanaWallets.filter((_, index) => index != idx);
     setSolanaWallets(updatedWallets);
+  };
+
+  const getSolanaBalance = async (publicKey: string) => {
+    try {
+      const url =
+        "https://solana-mainnet.g.alchemy.com/v2/VyVawzJyVj8hThqvh2XxUIxJ1drDHbb5";
+      const reqBody = {
+        jsonrpc: "2.0",
+        id: 1,
+        method: "getBalance",
+        params: [publicKey],
+      };
+      const response = await axios.post(url, reqBody);
+      const balanceInLamports = response.data.result.value;
+      return balanceInLamports / 1e9; // Convert lamports to SOL}
+    } catch (error) {
+      console.error("Error fetching balance:", error);
+      return 0;
+    }
   };
 
   return (
@@ -70,7 +93,9 @@ const SolanaWalletGenerator = ({ seed }: seedProps) => {
             <div className={styles.card_label}>Public Key:</div>
             <div className={styles.card_value}>{solanaWallet.publicKey}</div>
           </div>
-          <div className={styles.balance}>Balance: $ 0.00</div>
+          <div className={styles.balance}>
+            Balance: {solanaWallet.balance.toString()} SOL
+          </div>
         </div>
       ))}
     </div>
